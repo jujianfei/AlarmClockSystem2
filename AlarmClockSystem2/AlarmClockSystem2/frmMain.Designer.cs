b5edbc4f136c537c3cf8b2ec609e89b569c949e7
Micheal <15732625435@163.com>
2017-03-22 14:53:24 +0800

    basic alarm finished

---
+++ AlarmClockSystem2/AlarmClockSystem2/frmMain.Designer.cs
@@ -0,0 +1,438 @@
+﻿namespace AlarmClockSystem2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnReSet = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbSecond = new System.Windows.Forms.ComboBox();
+            this.cmbMinute = new System.Windows.Forms.ComboBox();
+            this.cmbHour = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimeRemain = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnReSet
+            // 
+            this.btnReSet.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReSet.Location = new System.Drawing.Point(178, 226);
+            this.btnReSet.Name = "btnReSet";
+            this.btnReSet.Size = new System.Drawing.Size(82, 29);
+            this.btnReSet.TabIndex = 31;
+            this.btnReSet.Text = "重新设定";
+            this.btnReSet.UseVisualStyleBackColor = true;
+            this.btnReSet.Visible = false;
+            this.btnReSet.Click += new System.EventHandler(this.btnReSet_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.Location = new System.Drawing.Point(328, 175);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 16);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "S";
+            this.label10.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(279, 175);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 16);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "M";
+            this.label9.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(228, 175);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 16);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "H";
+            this.label8.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(350, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 16);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "秒";
+            this.label7.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(301, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 16);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "分";
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(250, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "时";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(143, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "剩余时间：";
+            this.label4.Visible = false;
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.BackColor = System.Drawing.Color.Transparent;
+            this.lblTip.Font = new System.Drawing.Font("隶书", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTip.Location = new System.Drawing.Point(204, 77);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(109, 19);
+            this.lblTip.TabIndex = 28;
+            this.lblTip.Text = "闹铃已启动";
+            this.lblTip.Visible = false;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOk.Location = new System.Drawing.Point(271, 226);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 29);
+            this.btnOk.TabIndex = 16;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(383, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "秒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(278, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "分";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(175, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "时";
+            // 
+            // cmbSecond
+            // 
+            this.cmbSecond.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbSecond.FormattingEnabled = true;
+            this.cmbSecond.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cmbSecond.Location = new System.Drawing.Point(308, 126);
+            this.cmbSecond.Name = "cmbSecond";
+            this.cmbSecond.Size = new System.Drawing.Size(60, 24);
+            this.cmbSecond.TabIndex = 20;
+            // 
+            // cmbMinute
+            // 
+            this.cmbMinute.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbMinute.FormattingEnabled = true;
+            this.cmbMinute.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cmbMinute.Location = new System.Drawing.Point(208, 126);
+            this.cmbMinute.Name = "cmbMinute";
+            this.cmbMinute.Size = new System.Drawing.Size(60, 24);
+            this.cmbMinute.TabIndex = 18;
+            // 
+            // cmbHour
+            // 
+            this.cmbHour.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbHour.FormattingEnabled = true;
+            this.cmbHour.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24"});
+            this.cmbHour.Location = new System.Drawing.Point(109, 126);
+            this.cmbHour.Name = "cmbHour";
+            this.cmbHour.Size = new System.Drawing.Size(60, 24);
+            this.cmbHour.TabIndex = 17;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeRemain
+            // 
+            this.TimeRemain.Tick += new System.EventHandler(this.TimeRemain_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AlarmClockSystem2.Properties.Resources.BackImage;
+            this.ClientSize = new System.Drawing.Size(516, 332);
+            this.Controls.Add(this.btnReSet);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSecond);
+            this.Controls.Add(this.cmbMinute);
+            this.Controls.Add(this.cmbHour);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "闹铃";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReSet;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbSecond;
+        private System.Windows.Forms.ComboBox cmbMinute;
+        private System.Windows.Forms.ComboBox cmbHour;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer TimeRemain;
+    }
+}
+
---
+++ AlarmClockSystem2/AlarmClockSystem2/frmMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlarmClockSystem2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //判断设定时间是否大于现在的时间
+
+            //获取现在的时间
+            DateTime now = DateTime.Now;
+            //获取设定的时间
+            DateTime utime = Convert.ToDateTime(cmbHour.Text + ":" + cmbMinute.Text + ":" + cmbSecond.Text);
+            //计算时间差
+            TimeSpan ts = utime - now;
+            if (Convert.ToInt32(ts.TotalHours) < 0 || Convert.ToInt32(ts.TotalMinutes) < 0 || Convert.ToInt32(ts.TotalSeconds) < 0)
+            {
+                MessageBox.Show("设定时间小于当前时间，请重新设定！", "温馨提示");
+            }
+            else
+            {
+                ShowMsg show = new ShowMsg();
+
+                lblTip.Visible = true;
+                cmbHour.Enabled = false; cmbMinute.Enabled = false; cmbSecond.Enabled = false;
+
+                label4.Visible = true; label5.Visible = true; label6.Visible = true;
+                label7.Visible = true; label8.Visible = true; label9.Visible = true; label10.Visible = true;
+                timer1.Enabled = true;
+                TimeRemain.Enabled = true;
+
+                show.ShowMessageBoxTimeout("时间设定成功！", "温馨提示", MessageBoxButtons.OK, 1000);
+            }
+            btnReSet.Visible = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //负责计算
+        {
+            DateTime dt = DateTime.Now;
+            string hour = dt.Hour.ToString();
+            string minute = dt.Minute.ToString();
+            string second = dt.Second.ToString();
+
+            if (cmbHour.Text.Trim() == hour.Trim() && Convert.ToInt32(cmbMinute.Text.Trim()) <= Convert.ToInt32(minute.Trim()) && Convert.ToInt32(cmbSecond.Text.Trim()) <= Convert.ToInt32(second.Trim()))
+            {
+                timer1.Enabled = false;
+                TimeRemain.Enabled = false;
+                SoundPlayer player = new SoundPlayer("../../Resource/8236.wav");
+                player.Play();
+
+                MethodInvoker mshow = new MethodInvoker(msgshow);
+                this.BeginInvoke(mshow);
+            }
+        }
+        private void msgshow()
+        {
+            MessageBox.Show("时间到！", "温馨提示");
+        }
+
+        private void TimeRemain_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string hour = dt.Hour.ToString();
+            string minute = dt.Minute.ToString();
+            string second = dt.Second.ToString();
+
+            label8.Text = (Convert.ToInt32(cmbHour.Text) - Convert.ToInt32(hour)).ToString();
+
+            if (Convert.ToInt32(cmbSecond.Text) < Convert.ToInt32(second))
+            {
+                label9.Text = (Convert.ToInt32(cmbMinute.Text) - Convert.ToInt32(minute) - 1).ToString();
+                label10.Text = (Convert.ToInt32(cmbSecond.Text) + 60 - Convert.ToInt32(second)).ToString();
+            }
+            else
+            {
+                label9.Text = (Convert.ToInt32(cmbMinute.Text) - Convert.ToInt32(minute)).ToString();
+                label10.Text = (Convert.ToInt32(cmbSecond.Text) - Convert.ToInt32(second)).ToString();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            cmbHour.Text = dt.Hour.ToString();
+            cmbMinute.Text = dt.Minute.ToString();
+            cmbSecond.Text = dt.Second.ToString();
+        }
+
+        private void btnReSet_Click(object sender, EventArgs e)
+        {
+            cmbHour.Enabled = true;
+            cmbMinute.Enabled = true;
+            cmbSecond.Enabled = true;
+            lblTip.Visible = false;
+        }
+    }
+}
